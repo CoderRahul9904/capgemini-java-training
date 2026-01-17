4771e62af73e0b174f3746f723ad75c48982a37b
Rahul <mouryarahul543@gmail.com>
2026-01-17 12:21:31 +0530

    day7

---
+++ src/day7/AdvanceExcepHandle.java
@@ -0,0 +1,18 @@
+package day7;
+
+public class AdvanceExcepHandle {
+    public static int add(int a, int b) {
+        int c=0;
+        try{
+            c=a/b;
+            return c;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }finally{
+            return a;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(add(10,0));
+    }
+}
