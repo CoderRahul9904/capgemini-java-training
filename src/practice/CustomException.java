7895db197acd678d13f08da9e1a20f8d04d78c4c
Rahul <mouryarahul543@gmail.com>
2026-02-10 12:33:24 +0530

    Custom Exception

--- src/practice/CustomException.java
+++ src/practice/CustomException.java
@@ -1,4 +1,21 @@
 package practice;
 
+class NotInLoveException extends Exception{
+    public NotInLoveException(String msg){
+        super(msg);
+    }
+}
 public class CustomException {
+    public static void main(String[] args) {
+        int x=50;
+        try{
+            if(x > 80){
+                System.out.println("She is in Love");
+            }else{
+                throw new NotInLoveException("She is not in Love");
+            }
+        }catch(NotInLoveException e){
+            System.out.println(e);
+        }
+    }
 }
