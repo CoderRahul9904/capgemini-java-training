5d53eab76c990ffbbb78220fe658863f4e4bd1f7
Rahul <mouryarahul543@gmail.com>
2026-02-10 12:33:36 +0530

    exception basic

--- src/practice/ExceptionHandling.java
+++ src/practice/ExceptionHandling.java
@@ -1,4 +1,25 @@
 package practice;
 
 public class ExceptionHandling {
+    public static void main(String[] args) {
+        try{
+            System.out.println(9/0);
+        }
+        catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }
+        catch(NullPointerException e){
+            System.out.println(e.getMessage());
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
