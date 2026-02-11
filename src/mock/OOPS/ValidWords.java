c7e3f94ec03f695b6f26ad7d4106765d808fa353
Rahul <mouryarahul543@gmail.com>
2026-02-11 22:17:12 +0530

    will solve later

---
+++ src/mock/OOPS/ValidWords.java
@@ -0,0 +1,15 @@
+package mock.OOPS;
+
+import java.util.*;
+
+public class ValidWords {
+    public int countValidWords(String sentence) {
+        // TODO
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        ValidWords v = new ValidWords();
+        System.out.println(v.countValidWords("cat and  dog")); // sample
+    }
+}
